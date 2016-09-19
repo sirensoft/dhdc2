42cf4eaf3fbf5995d8a8eb904935fd1910c66ceb
tehnn <tehnnn@gmail.com>
2016-09-19 23:39:35 +0700

    utehn-new-init

--- frontend/modules/gis/controllers/DefaultController.php
+++ frontend/modules/gis/controllers/DefaultController.php
@@ -29,7 +29,7 @@ class DefaultController extends AppController {
         $this->layout = 'gis';
         $config_main = Sysconfigmain::find()->one();
         $amp = $config_main->district_code;
-        
+
         //tambon
         $sql = " select * from gis_dhdc where concat(PROV_CODE,AMP_CODE)='$amp'";
         $raw = \Yii::$app->db->createCommand($sql)->queryAll();
@@ -48,7 +48,6 @@ class DefaultController extends AppController {
         }
         $tambon_json = json_encode($tambon_json);
         // end tambon
-        
         // Hos
         $sql = " SELECT h.hosname,concat(t.hcode,'-',h.hosname) hos,t.lat,t.lon from geojson t 
                  INNER JOIN chospital_amp h ON t.hcode = h.hoscode ";
@@ -76,13 +75,13 @@ class DefaultController extends AppController {
         ]);
     }
 
-    public function actionHouse(){
+    public function actionHouse() {
         $this->permitRole([1, 2]);
         $this->layout = 'gis';
         $config_main = Sysconfigmain::find()->one();
         $amp = $config_main->district_code;
-        
-          //tambon
+
+        //tambon
         $sql = " select * from gis_dhdc where concat(PROV_CODE,AMP_CODE)='$amp'";
         $raw = \Yii::$app->db->createCommand($sql)->queryAll();
         $tambon_json = [];
@@ -100,7 +99,6 @@ class DefaultController extends AppController {
         }
         $tambon_json = json_encode($tambon_json);
         // end tambon
-        
         // Hos
         $sql = " SELECT h.hosname,concat(t.hcode,'-',h.hosname) hos,t.lat,t.lon from geojson t 
                  INNER JOIN chospital_amp h ON t.hcode = h.hoscode ";
@@ -111,7 +109,7 @@ class DefaultController extends AppController {
                 'type' => 'Feature',
                 'properties' => [
                     'HOS' => $value['hos'],
-                    //'SEARCH_TEXT' => $value['hosname'],
+                //'SEARCH_TEXT' => $value['hosname'],
                 ],
                 'geometry' => [
                     'type' => 'Point',
@@ -121,7 +119,6 @@ class DefaultController extends AppController {
         }
         $hos_json = json_encode($hos_json);
         // end Hos
-        
         //House
         $sql = " SELECT concat(t.HOUSE,' ม.',t.VILLAGE,' ต.',t.TAMBON_NAMT) FULL_HOUSE,t.HEAD_NAME,t.LATITUDE,t.LONGITUDE from t_house_gis t WHERE t.GIS = 'Y' ";
         $raw = \Yii::$app->db->createCommand($sql)->queryAll();
@@ -133,31 +130,29 @@ class DefaultController extends AppController {
                     'FULL_HOUSE' => $value['FULL_HOUSE'],
                     'HEAD_NAME' => $value['HEAD_NAME'],
                     'SEARCH_TEXT' => $value['FULL_HOUSE'],
-                    
                 ],
                 'geometry' => [
                     'type' => 'Point',
-                    'coordinates' => [$value['LONGITUDE'] * 1,$value['LATITUDE'] * 1],
+                    'coordinates' => [$value['LONGITUDE'] * 1, $value['LATITUDE'] * 1],
                 ]
             ];
         }
         $house_json = json_encode($house_json);
         // end House
-        
+
         return $this->render('house', [
                     'tambon_json' => $tambon_json,
                     'house_json' => $house_json,
-                    'hos_json'=>$hos_json
-                   
+                    'hos_json' => $hos_json
         ]);
     }
-    
-     public function actionHouseFind(){
+
+    public function actionHouseFind() {
         $this->permitRole([1, 2]);
         $this->layout = 'gis';
         $config_main = Sysconfigmain::find()->one();
         $amp = $config_main->district_code;
-          //tambon
+        //tambon
         $sql = " select * from gis_dhdc where concat(PROV_CODE,AMP_CODE)='$amp'";
         $raw = \Yii::$app->db->createCommand($sql)->queryAll();
         $tambon_json = [];
@@ -175,7 +170,6 @@ class DefaultController extends AppController {
         }
         $tambon_json = json_encode($tambon_json);
         // end tambon
-        
         // Hos
         $sql = " SELECT h.hosname,concat(t.hcode,'-',h.hosname) hos,t.lat,t.lon from geojson t 
                  INNER JOIN chospital_amp h ON t.hcode = h.hoscode ";
@@ -186,7 +180,7 @@ class DefaultController extends AppController {
                 'type' => 'Feature',
                 'properties' => [
                     'HOS' => $value['hos'],
-                    //'SEARCH_TEXT' => $value['hosname'],
+                //'SEARCH_TEXT' => $value['hosname'],
                 ],
                 'geometry' => [
                     'type' => 'Point',
@@ -196,7 +190,6 @@ class DefaultController extends AppController {
         }
         $hos_json = json_encode($hos_json);
         // end Hos
-        
         //House
         $sql = " SELECT concat(t.HOUSE,' ม.',t.VILLAGE,' ต.',t.TAMBON_NAMT) FULL_HOUSE,t.HEAD_NAME,t.LATITUDE,t.LONGITUDE from t_house_gis t WHERE t.GIS = 'Y' ";
         $raw = \Yii::$app->db->createCommand($sql)->queryAll();
@@ -208,21 +201,92 @@ class DefaultController extends AppController {
                     'FULL_HOUSE' => $value['FULL_HOUSE'],
                     'HEAD_NAME' => $value['HEAD_NAME'],
                     'SEARCH_TEXT' => $value['FULL_HOUSE'],
-                    
                 ],
                 'geometry' => [
                     'type' => 'Point',
-                    'coordinates' => [$value['LONGITUDE'] * 1,$value['LATITUDE'] * 1],
+                    'coordinates' => [$value['LONGITUDE'] * 1, $value['LATITUDE'] * 1],
                 ]
             ];
         }
         $house_json = json_encode($house_json);
         // end House
-        
+
         return $this->render('house-find', [
                     'tambon_json' => $tambon_json,
                     'house_json' => $house_json,
-                    'hos_json'=>$hos_json                   
+                    'hos_json' => $hos_json
         ]);
     }
+    
+    public function actionDisease($disease=NULL) {
+        $this->permitRole([1, 2]);
+        $this->layout = 'gis';
+        $config_main = Sysconfigmain::find()->one();
+        $amp = $config_main->district_code;
+        //tambon
+        $sql = " select * from gis_dhdc where concat(PROV_CODE,AMP_CODE)='$amp'";
+        $raw = \Yii::$app->db->createCommand($sql)->queryAll();
+        $tambon_json = [];
+        foreach ($raw as $value) {
+            $tambon_json[] = [
+                'type' => 'Feature',
+                'properties' => [
+                    'TAM_NAMT' => "ต." . $value['TAM_NAMT'],
+                ],
+                'geometry' => [
+                    'type' => 'MultiPolygon',
+                    'coordinates' => json_decode($value['COORDINATES']),
+                ]
+            ];
+        }
+        $tambon_json = json_encode($tambon_json);
+        // end tambon
+        // Hos
+        $sql = " SELECT h.hosname,concat(t.hcode,'-',h.hosname) hos,t.lat,t.lon from geojson t 
+                 INNER JOIN chospital_amp h ON t.hcode = h.hoscode ";
+        $raw = \Yii::$app->db->createCommand($sql)->queryAll();
+        $hos_json = [];
+        foreach ($raw as $value) {
+            $hos_json[] = [
+                'type' => 'Feature',
+                'properties' => [
+                    'HOS' => $value['hos'],
+                //'SEARCH_TEXT' => $value['hosname'],
+                ],
+                'geometry' => [
+                    'type' => 'Point',
+                    'coordinates' => [$value['lon'] * 1, $value['lat'] * 1],
+                ]
+            ];
+        }
+        $hos_json = json_encode($hos_json);
+        // end Hos
+        //House
+        $sql = " SELECT concat(t.HOUSE,' ม.',t.VILLAGE,' ต.',t.TAMBON_NAMT) FULL_HOUSE,t.HEAD_NAME,t.LATITUDE,t.LONGITUDE from t_house_gis t WHERE t.GIS = 'Y' ";
+        $raw = \Yii::$app->db->createCommand($sql)->queryAll();
+        $house_json = [];
+        foreach ($raw as $value) {
+            $house_json[] = [
+                'type' => 'Feature',
+                'properties' => [
+                    'FULL_HOUSE' => $value['FULL_HOUSE'],
+                    'HEAD_NAME' => $value['HEAD_NAME'],
+                    'SEARCH_TEXT' => $value['FULL_HOUSE'],
+                ],
+                'geometry' => [
+                    'type' => 'Point',
+                    'coordinates' => [$value['LONGITUDE'] * 1, $value['LATITUDE'] * 1],
+                ]
+            ];
+        }
+        $house_json = json_encode($house_json);
+        // end House
+
+        return $this->render('disease', [
+                    'tambon_json' => $tambon_json,
+                    'house_json' => $house_json,
+                    'hos_json' => $hos_json
+        ]);
+    }
+
 }
---
+++ frontend/modules/gis/views/default/disease.php
@@ -0,0 +1,177 @@
+<?php
+$this->title = "DHDC2:GIS";
+$this->registerCssFile('https://api.mapbox.com/mapbox.js/v2.4.0/mapbox.css', ['async' => false, 'defer' => true]);
+$this->registerCssFile('./lib-gis/leaflet-search.min.css',['async' => false, 'defer' => true]);
+$this->registerCssFile('./lib-gis/leaflet.label.css',['async' => false, 'defer' => true]);
+
+$this->registerJsFile('https://api.mapbox.com/mapbox.js/v2.4.0/mapbox.js', ['position' => $this::POS_HEAD]);
+$this->registerJsFile('./lib-gis/leaflet-search.min.js',['position' => $this::POS_HEAD]);
+$this->registerJsFile('./lib-gis/leaflet.label.js',['position' => $this::POS_HEAD]);
+
+
+?>
+
+
+<div class="panel panel-info">
+    <div class="panel-heading">
+        <b>แผนที่แสดงการเจ็บป่วยด้วยโรค.....</b>-[แฟ้ม DIAGNOSIS_OPD(IPD)]       
+    </div>
+    <div class="panel-body" >
+        <div id="map" style="width: 100%;height: 75vh;"></div>   
+    </div>
+    <div class="panel-footer" id="info">
+        <b><u>แผนที่ขอบเขตระดับหมู่บ้าน</u></b> กรุณาติดต่อ <a href="https://www.facebook.com/tehnn" target="_blank">ผู้พัฒนา</a>
+        <?php
+       // echo $house_json;
+        ?>
+        
+    </div>
+</div>
+
+
+<?php
+
+$js = <<<JS
+             
+    L.mapbox.accessToken = 'pk.eyJ1IjoidGVobm5uIiwiYSI6ImNpZzF4bHV4NDE0dTZ1M200YWxweHR0ZzcifQ.lpRRelYpT0ucv1NN08KUWQ';
+   
+    var map = L.map('map');
+        
+   
+        
+     var baseLayers = {
+	"แผนที่ถนน": L.mapbox.tileLayer('mapbox.streets').addTo(map),        
+        "แผนที่ดาวเทียม": L.mapbox.tileLayer('mapbox.satellite'),
+        
+    };
+  
+    
+    
+        
+    var _group1 = L.layerGroup();
+    var _group2 = L.layerGroup();
+    var _group3 = L.layerGroup();
+     
+   //ตำบล
+    var tam_layer=L.geoJson($tambon_json,{
+        style:style,
+        onEachFeature:function(feature,layer){         
+            //layer.bindPopup(feature.properties.TAM_NAMT);
+            layer.bindLabel(feature.properties.TAM_NAMT);
+            layer.on({
+                    mouseover: highlightFeatureTamLayer,
+                    mouseout: resetHighlightTamLayer,
+                    click: zoomToFeature
+                });
+         },
+         
+       }).addTo(_group1);
+    map.fitBounds(tam_layer.getBounds());
+    // จบตำบล
+    
+    //house
+    var ic_house = L.icon({
+        iconUrl: './images/ic_house.png',
+        iconSize:     [28, 28],    
+        iconAnchor:   [20, 20],   
+        popupAnchor:  [-0, -20] 
+    });
+       
+    var house_layer =L.geoJson($house_json,{                
+            
+           onEachFeature:function(feature,layer){    
+                //layer.setIcon(L.mapbox.marker.icon({'marker-color': '#82f217','marker-symbol':'h'})); 
+                layer.setIcon(ic_house); 
+                layer.bindPopup(feature.properties.FULL_HOUSE+'<br>'+'เจ้าบ้าน:-'+feature.properties.HEAD_NAME);               
+                
+               
+           },
+           
+    }).addTo(_group2);
+   
+    // จบ house
+        
+    //hos
+     var hos_layer =L.geoJson($hos_json,{                
+            
+           onEachFeature:function(feature,layer){    
+                layer.setIcon(L.mapbox.marker.icon({'marker-color': '#09945f','marker-symbol':'h'})); 
+                layer.bindPopup(feature.properties.HOS);
+                //layer.bindLabel(feature.properties.HOS);
+                
+               
+           },
+           
+    }).addTo(_group3);
+    //จบ hos
+        
+    var overlays = {   
+        
+        "ขอบเขตตำบล": _group1.addTo(map),
+        "บ้านผู้ป่วย": _group2,
+        "หน่วยบริการ":_group3
+        
+               
+    };
+        
+    L.control.layers(baseLayers,overlays).addTo(map);
+      
+    //search
+    var searchControl = new L.Control.Search({
+		layer: house_layer,
+		propertyName: 'SEARCH_TEXT',
+		circleLocation: false,
+		
+    });
+
+    searchControl.on('search:locationfound', function(e) {
+				
+		if(e.layer._popup)e.layer.openPopup();
+
+    }).on('search:collapsed', function(e) {
+
+		house_layer.eachLayer(function(layer) {	
+			house_layer.resetStyle(layer);
+		});	
+    });
+    //map.addControl( searchControl );  
+    
+    // other function    
+    function style(feature) {
+        return {
+            fillColor: '#4169E1',
+            weight: 2,
+            opacity: 1,
+            color: 'white',
+            dashArray: '3',
+            fillOpacity: 0.7
+        }
+    } 
+        
+    function highlightFeatureTamLayer(e) {
+        var layer = e.target;
+        layer.setStyle({
+            weight: 5,
+            color: '#B5E61D',
+            dashArray: '',
+            fillOpacity: 0.7
+        });
+        if (!L.Browser.ie && !L.Browser.opera) {
+            layer.bringToFront();
+        }
+        
+    }
+    function resetHighlightTamLayer(e) {
+        tam_layer.resetStyle(e.target);
+        
+    }
+    function zoomToFeature(e) {
+        map.fitBounds(e.target.getBounds());
+    }
+        
+JS;
+$this->registerJs($js);
+?>
+
+
+
