0f7c3c619ad16a2d1f6f4906a043baee6a334b8b
tehnn <tehnnn@gmail.com>
2016-09-19 13:18:45 +0700

    utehn-new-init

--- frontend/modules/gis/controllers/DefaultController.php
+++ frontend/modules/gis/controllers/DefaultController.php
@@ -14,70 +14,63 @@ class DefaultController extends AppController {
 
     public function actionTest() {
         $this->layout = 'gis';
-        $this->permitRole([1,2]);
+        $this->permitRole([1, 2]);
         $this->identify_key();
-        
+
         return $this->render('dhf');
     }
-    
-    public function actionTestKeys(){
+
+    public function actionTestKeys() {
         
     }
-     public function actionHos(){
+
+    public function actionHos() {
         $this->layout = 'gis';
         $config_main = Sysconfigmain::find()->one();
         $amp = $config_main->district_code;
-        // Tambon
-        //$amp ='6301';
+
         $sql = " select * from gis_dhdc where concat(PROV_CODE,AMP_CODE)='$amp'";
         $raw = \Yii::$app->db->createCommand($sql)->queryAll();
-        $tambon_json =[];
+        $tambon_json = [];
         foreach ($raw as $value) {
-            $tambon_json[]=[
-                'type'=>'Feature',
-                'properties'=>[
-                    'TAM_NAMT'=>"ต.".$value['TAM_NAMT'],
-                    
+            $tambon_json[] = [
+                'type' => 'Feature',
+                'properties' => [
+                    'TAM_NAMT' => "ต." . $value['TAM_NAMT'],
                 ],
-                'geometry'=>[
-                    'type'=>'MultiPolygon',
-                    'coordinates'=>json_decode($value['COORDINATES']),                    
+                'geometry' => [
+                    'type' => 'MultiPolygon',
+                    'coordinates' => json_decode($value['COORDINATES']),
                 ]
             ];
         }
         $tambon_json = json_encode($tambon_json);
         // end tambon
-        
-        
-              // Hos
+        // Hos
         $sql = " SELECT h.hosname,concat(t.hcode,'-',h.hosname) hos,t.lat,t.lon from geojson t 
                  INNER JOIN chospital_amp h ON t.hcode = h.hoscode ";
         $raw = \Yii::$app->db->createCommand($sql)->queryAll();
-        $hos_json =[];
+        $hos_json = [];
         foreach ($raw as $value) {
-            $hos_json[]=[
-                'type'=>'Feature',
-                'properties'=>[
-                    'HOS'=>$value['hos'],
-                    'SEARCH_TEXT'=>$value['hosname'],
-                    
+            $hos_json[] = [
+                'type' => 'Feature',
+                'properties' => [
+                    'HOS' => $value['hos'],
+                    'SEARCH_TEXT' => $value['hosname'],
                 ],
-                'geometry'=>[
-                    'type'=>'Point',
-                    'coordinates'=>[$value['lon']*1,$value['lat']*1],                    
+                'geometry' => [
+                    'type' => 'Point',
+                    'coordinates' => [$value['lon'] * 1, $value['lat'] * 1],
                 ]
             ];
         }
         $hos_json = json_encode($hos_json);
         // end Hos
-        
-        
-        
-        return $this->render('hos',[
-            'tambon_json'=>$tambon_json,
-            'hos_json'=>$hos_json
+
+        return $this->render('hos', [
+                    'tambon_json' => $tambon_json,
+                    'hos_json' => $hos_json
         ]);
-        
     }
 
 }
