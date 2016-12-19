ae24324eaf948c965780d8d4bb178d43363eebf3
tehnn <tehnnn@gmail.com>
2016-12-19 23:42:35 +0700

    utehn-4-train

--- frontend/modules/tst/views/default/report-items.php
+++ frontend/modules/tst/views/default/report-items.php
@@ -3,3 +3,8 @@ $this->title = "ระบบประเมินผลงาน(เวอร์
 $this->params['breadcrumbs'][] = ['url'=>['/tst'],'label'=>'กิจกรรมสาธารณสุข'];
 $this->params['breadcrumbs'][] = $group;
 
+$sql = "select * from tst_citems where cgroup_id in ($id)";
+$raw = \Yii::$app->db->createCommand($sql)->queryAll();
+echo "<pre>";
+print_r($raw);
+
