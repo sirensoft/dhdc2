9212deec2cc6d31cf26e9dd27caa2ebd857ea3fc
tehnn <tehnnn@gmail.com>
2016-12-22 20:44:14 +0700

    utehn-4-train

--- frontend/modules/tst/controllers/DefaultController.php
+++ frontend/modules/tst/controllers/DefaultController.php
@@ -39,8 +39,27 @@ class DefaultController extends AppController {
         if($group_id==7)$searchModel = new \frontend\modules\tst\models\KpiGroup7();
         if($group_id==8)$searchModel = new \frontend\modules\tst\models\KpiGroup8();
         if($group_id==9)$searchModel = new \frontend\modules\tst\models\KpiGroup9();
+        if($group_id==10)$searchModel = new \frontend\modules\tst\models\KpiGroup10();
         
+        if($group_id==11)$searchModel = new \frontend\modules\tst\models\KpiGroup11();
+        if($group_id==12)$searchModel = new \frontend\modules\tst\models\KpiGroup12();
+        if($group_id==13)$searchModel = new \frontend\modules\tst\models\KpiGroup13();
+        if($group_id==14)$searchModel = new \frontend\modules\tst\models\KpiGroup14();
+        if($group_id==15)$searchModel = new \frontend\modules\tst\models\KpiGroup15();
+        if($group_id==16)$searchModel = new \frontend\modules\tst\models\KpiGroup16();
+        if($group_id==17)$searchModel = new \frontend\modules\tst\models\KpiGroup17();
+        if($group_id==18)$searchModel = new \frontend\modules\tst\models\KpiGroup18();
+        if($group_id==19)$searchModel = new \frontend\modules\tst\models\KpiGroup19();
+        if($group_id==20)$searchModel = new \frontend\modules\tst\models\KpiGroup20();
+        
+        if($group_id==21)$searchModel = new \frontend\modules\tst\models\KpiGroup21();
+        if($group_id==22)$searchModel = new \frontend\modules\tst\models\KpiGroup22();
+        if($group_id==23)$searchModel = new \frontend\modules\tst\models\KpiGroup23();
         if($group_id==24)$searchModel = new \frontend\modules\tst\models\KpiGroup24();
+        if($group_id==25)$searchModel = new \frontend\modules\tst\models\KpiGroup25();
+        if($group_id==26)$searchModel = new \frontend\modules\tst\models\KpiGroup26();
+        if($group_id==27)$searchModel = new \frontend\modules\tst\models\KpiGroup27();
+        if($group_id==28)$searchModel = new \frontend\modules\tst\models\KpiGroup28();
         
         
         
