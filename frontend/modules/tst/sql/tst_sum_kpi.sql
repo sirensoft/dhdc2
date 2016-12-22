dcf7938c4b50f23908d4375af82de4e435b3eff5
tehnn <tehnnn@gmail.com>
2016-12-22 13:44:38 +0700

    utehn-4-train

--- frontend/modules/tst/sql/tst_sum_kpi.sql
+++ frontend/modules/tst/sql/tst_sum_kpi.sql
@@ -1,11 +1,12 @@
 SELECT p.HOSPCODE,t.cid,p.`NAME`,p.LNAME,p.BIRTH,p.SEX,p.TYPEAREA
 ,amp.ampurname amp,tmb.tambonname tmb,RIGHT(p.vhid,2) vill,'' addr
 
-,(SELECT 'ok' FROM tst_kpi22 a WHERE a.cid=t.cid AND a.byear=2560) k
+,(SELECT 'yes' FROM tst_kpi44 a WHERE a.cid=t.cid AND a.byear=2560) k
+,(SELECT 'yes' FROM tst_kpi45 a WHERE a.cid=t.cid AND a.byear=2560) k
+,(SELECT 'yes' FROM tst_kpi46 a WHERE a.cid=t.cid AND a.byear=2560) k
 
 FROM tst_pop t 
 LEFT JOIN t_person_cid p on t.cid = p.cid
 LEFT JOIN campur amp on amp.ampurcodefull = LEFT(p.vhid,4)
 LEFT JOIN ctambon_amp tmb ON tmb.tamboncodefull = LEFT(p.vhid,6)
-WHERE FIND_IN_SET('12',t.pop_group) 
-
+WHERE FIND_IN_SET('27',t.pop_group)
